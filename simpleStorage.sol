bb20096424e72b572637662b7771e85acd2583ee
Khaled gamal <khaled2000s.kg@gmail.com>
2022-11-17 17:41:55 +0200

    add store and retrieve

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,5 +1,15 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage{
-    
+    int256 public favoriteNumber;
+
+    function store(uint256 _data) public {
+        favoriteNumber=_data;
+    }
+      
+    //view : just view data from the BC
+    //pure: use when doing some type of math operation  
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
 }
